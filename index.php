3214676f2d6c2f3dcc79a0f5e968381dcb5a2d33
Marcelo <mesquitaleal@gmail.com>
2016-06-28 17:04:36 -0300

    [NEW] First commit

---
+++ index.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Diálogos</title>
+	<link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="bootstrap/dist/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+	<nav>
+		<div class="nav-conferences">
+			Conferência
+			<a href="#conference-1"><span>1</span></a>
+			<a href="#conference-2"><span>2</span></a>
+			<a href="#conference-3"><span>3</span></a>
+			<a href="#conference-4"><span>4</span></a>
+			<a href="#conference-5"><span>5</span></a>
+			<a href="#conference-6"><span>6</span></a>
+		</div>
+	</nav>
+	<section class="conference-section" id="conference-1">
+		<!--<svg class="svg-circle" viewBox="0 0 100 100" preserveAspectRatio="none">
+			<circle cx="120" cy="0" r="100" />
+			<text class="svg-text" x="0" y="10">Junho 07</text>
+			<text class="svg-text text-crop" x="0" y="10">Junho 07</text>
+			<clipPath id="clipping">
+				<circle cx="120" cy="0" r="100"/>
+			</clipPath>
+		</svg>-->
+		<article class="content">
+			<div class="circle">
+				<img src="http://placehold.it/350x350">
+			</div>
+			<div class="info">
+				<h1 class="conference-date">JUNHO <span>07</span></h1>
+				<p class="conference-number">Conferência I</p>
+				<h2 class="conference-title">Festas Populares e Indústria do Entretenimento</h2>
+				<p class="conference-description">
+					A conferência visa abordar a emergência da lógica e práticas típicas do campo da economia que têm garantido aos setores culturais mais tradicionais a condição de grandes mercados. A palestra abordará setores de manifestações culturais no sentido amplo, como festas populares e a história do desenvolvimento do mercado de televisão no Brasil.
+				</p>
+				<p>Palestrantes:</p>
+				<div class="conference-panelist">
+					<img src="image/teste.jpg">
+					<p>Paulo Miguez<br/>Universidade Federal da Bahia</p>
+				</div>
+				<div class="conference-panelist">
+					<img src="image/teste.jpg">
+					<p>Fábio Sá Earp<br/>Universidade Federal do Rio de Janeiro</p>
+				</div>
+			</div>
+		</article>
+	</section>
+
+	<svg xmlns="http://www.w3.org/2000/svg" class="section-transition transition-to-2" version="1.1" viewBox="0 0 100 100" preserveAspectRatio="none">
+	    <path d="M0 100 C 20 0 50 0 100 100 Z" />
+	</svg>
+
+	<section class="conference-section section-2" id="conference-2">
+		<article class="content content-reverse">
+			<div class="info">
+				<h1 class="conference-date">JULHO 05</h1>
+				<p class="conference-number">Conferência II</p>
+				<h2 class="conference-title">Economia da Música e do Audiovisual: uma visão do mercado a partir de realizadores</h2>
+				<p class="conference-description">
+					A conferência irá abordar os setores do cinema e da música a partir da visão dos realizadores. Os convidados apresentarão um panorama do setor musical e audiovisual no Brasil, bem como os principais entraves e vantagens para a produção cultural contemporânea. 
+				</p>
+				<p>Palestrantes:</p>
+				<div class="conference-panelist">
+					<img src="image/teste.jpg">
+					<p>Carlos Gerbase<br/> Pontifícia Universidade Católica do Rio Grande do Sul</p>
+				</div>
+				<div class="conference-panelist">
+					<img src="image/teste.jpg">
+					<p>Pena Schimidt<br/>Universidade Federal do Rio Grande do Sul (Brasil)</p>
+				</div>
+			</div>
+			<div class="circle">
+				<img src="http://placehold.it/350x350">
+			</div>
+		</article>
+	</section>
+
+	<svg xmlns="http://www.w3.org/2000/svg" class="section-transition transition-to-3" version="1.1" viewBox="0 0 100 100" preserveAspectRatio="none">
+	    <path d="M0 0 C 50 100 80 100 100 0 Z" />
+	</svg>
+
+	<section class="conference-section section-2" id="conference-3">
+		<article class="container">
+			<div class="circle">
+				<img src="image/dance.png">
+			</div>
+			<h1 class="conference-date">AGOSTO 08</h1>
+			<p class="conference-number">Conferência III</p>
+			<h2 class="conference-title">Políticas Culturais: Reflexões Contemporâneas</h2>
+			<p class="conference-description">
+				Os conferencistas debaterão o contexto de políticas culturais e seus impactos sobre as realidades culturais locais a partir de um olhar das esferas pública e privada. A atividade apresentará discussões contemporâneas sobre a construção de políticas públicas de cultura a fim de traçar um panorama histórico do papel do Estado e da iniciativa privada nesse processo de diálogo com as questões da participação social, da cidadania e da diversidade cultural. 
+			</p>
+			<p>Palestrantes:</p>
+			<div class="conference-panelist">
+				<img src="image/teste.jpg">
+				<p>Lia Calabre<br/>Fundação Casa de Rui Barbosa</p>
+			</div>
+			<div class="conference-panelist">
+				<img src="image/teste.jpg">
+				<p>Francisco Marshall<br/>Universidade Federal do Rio Grande do Sul</p>
+			</div>
+		</article>
+	</section>
+
+	<svg xmlns="http://www.w3.org/2000/svg" class="section-transition transition-to-4" version="1.1" viewBox="0 0 100 100" preserveAspectRatio="none">
+	    <path d="M0 100 C40 0 60 0 100 100 Z" />
+	</svg>
+
+	<section class="conference-section section-2" id="conference-4">
+		<article class="container">
+			<div class="circle">
+				<img src="image/dance.png">
+			</div>
+			<h1 class="conference-date">SETEMBRO 13</h1>
+			<p class="conference-number">Conferência IV</p>
+			<h2 class="conference-title">Da nova economia à economia criativa</h2>
+			<p class="conference-description">
+				No centro das mudanças empreendidas pela chamada “nova economia” estão a economia colaborativa, as tecnologia da informação e os novas formas de trabalho. Assim, a conferência debaterá as novas condições de produção e circulação de bens culturais e criativos na sociedade contemporânea a partir dos dispositivos digitais e em rede, compreendendo a centralidade assumida pelas tecnologias na superação de modelos assimétricos de difusão. 
+			</p>
+			<p>Palestrantes:</p>
+			<div class="conference-panelist">
+				<img src="image/teste.jpg">
+				<p>Glaucia Campregher<br/>Universidade Federal do Rio Grande do Sul</p>
+			</div>
+			<div class="conference-panelist">
+				<img src="image/teste.jpg">
+				<p>Messias Bandeira<br/>Universidade Federal da Bahia</p>
+			</div>
+		</article>
+	</section>
+
+	<svg xmlns="http://www.w3.org/2000/svg" class="section-transition transition-to-5" version="1.1" viewBox="0 0 100 100" preserveAspectRatio="none">
+	    <path d="M0 0 C 40 100 60 100 100 0 Z" />
+	</svg>
+
+
+	<section class="conference-section section-2" id="conference-5">
+		<article class="container">
+			<div class="circle">
+				<img src="image/dance.png">
+			</div>
+			<h1 class="conference-date">OUTUBRO 25</h1>
+			<p class="conference-number">Conferência V</p>
+			<h2 class="conference-title">A Cultura na Era Global </h2>
+			<p class="conference-description">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</p>
+			<p>Palestrantes:</p>
+			<div class="conference-panelist">
+				<img src="image/teste.jpg">
+				<p>Francisco Saboya<br/>Porto Digital - Recife</p>
+			</div>
+			<div class="conference-panelist">
+				<img src="image/teste.jpg">
+				<p>Marcos André Carvalho<br/>Rio Criativo</p>
+			</div>
+		</article>
+	</section>
+
+	<svg xmlns="http://www.w3.org/2000/svg" class="section-transition transition-to-6" version="1.1" viewBox="0 0 100 100" preserveAspectRatio="none">
+	    <path d="M0 100 C 20 0 50 0 100 100 Z" />
+	</svg>
+
+	<section class="conference-section section-2" id="conference-6">
+		<article class="container">
+			<div class="circle">
+				<img src="image/dance.png">
+			</div>
+			<h1 class="conference-date">NOVEMBRO </h1>
+			<p class="conference-number">Conferência VI</p>
+			<h2 class="conference-title">Economia Criativa e comércio internacional: novos modelos de negócios</h2>
+			<p class="conference-description">
+				A conferência visa proporcionar um debate sobre os fluxos comerciais internacionais de bens e serviços oriundos das indústrias criativas, bem como os modelos de negócios associados a estes movimentos de internacionalização. De forma comparativa Brasil-França, serão apresentados estudos e indicadores que avaliam os impactos econômicos produzidos pelo comércio internacional pelas indústrias criativas. 
+			</p>
+			<p>Palestrantes:</p>
+			<div class="conference-panelist">
+				<img src="image/teste.jpg">
+				<p>Leandro Valiati<br/>Universidade Federal do Rio Grande do Sul (Brasil)</p>
+			</div>
+			<div class="conference-panelist">
+				<img src="image/teste.jpg">
+				<p>François Moreau<br/>Université Paris XIII</p>
+			</div>
+			<div class="conference-panelist">
+				<img src="image/teste.jpg">
+				<p>Paul Heritage<br/>Queen Mary University</p>
+			</div>
+		</article>
+	</section>
+</body>
+</html>
